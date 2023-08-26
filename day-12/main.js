5379ece1df121c34a91230d221c92185168e109c
misterdan100 <danielmca60@gmail.com>
2023-08-25 23:39:41 -0500

    Finishing Movies/Series Search Engine with json, fetch and filters on JS

--- day-12/main.js
+++ day-12/main.js
@@ -2,31 +2,37 @@ const selectCategories = document.querySelector('#selectCategories');
 const inputBuscar = document.querySelector('#inputBuscar');
 const btnBuscar = document.querySelector('#btnBuscar');
 const ulContainer = document.querySelector('#ulContainer');
-const liCard = document.querySelector('li');
 
 selectCategories.addEventListener('change', seleccionarDatos);
 inputBuscar.addEventListener('keydown', validarInput);
 btnBuscar.addEventListener('click', validarParametros);
-// liCard.addEventListener('mouseout', ocultarSinopsis)
+inputBuscar.addEventListener('input', validarParametros);
 
 let datosMostrar;
 
 function seleccionarDatos(e) {
-  if (e.target.value === 'movies') {
-    fetch('movies.json')
-      .then(res => res.json())
-      .then(data => datosMostrar = data.data)
-      .catch(error => console.log(error))
-  } else {
-    fetch('series.json')
-      .then(res => res.json())
-      .then(data => datosMostrar = data.data)
-      .catch(error => console.log(error))
-  };
 
   //* Evento personalizado 
   let nuevoEvento = new CustomEvent('categoriaElegida');
   selectCategories.dispatchEvent(nuevoEvento);
+
+    if (e.target.value === 'movies') {
+      fetch('movies.json')
+        .then(res => res.json())
+        .then((data) => {
+          datosMostrar = data.data;
+          mostrarResultados(datosMostrar);
+        })
+        .catch(error => console.log(error))
+    } else if (e.target.value === 'series') {
+      fetch('series.json')
+        .then(res => res.json())
+        .then(data => {
+          datosMostrar = data.data;
+          mostrarResultados(datosMostrar);
+        })
+        .catch(error => console.log(error))
+    };
 }
 
 selectCategories.addEventListener('categoriaElegida', mostrarCategoria);
@@ -44,15 +50,18 @@ function validarInput(e) {
     return
   } else if (e.keyCode >= 65 && e.keyCode <= 90) {
     return
+  } else if (e.keyCode === 13) {
+    validarParametros()
   } else {
     e.preventDefault();
   }
 }
 
 function validarParametros() {
-  if( selectCategories.value === '') {
+  if (selectCategories.value === '') {
     mostrarAlerta('Seleccione una Categoria');
-  } else if( inputBuscar.value === '') {
+  } else if (inputBuscar.value === '') {
+    ulContainer.textContent = '';
     mostrarAlerta('Escriba un termino de busqueda');
   } else {
     filtrarBusqueda(inputBuscar.value)
@@ -60,49 +69,52 @@ function validarParametros() {
 }
 
 function filtrarBusqueda(terminoBusqueda) {
-  console.log(datosMostrar);
-  const resultados = datosMostrar.filter( video => {
+  const resultados = datosMostrar.filter(video => {
     const termino = terminoBusqueda.toLowerCase();
     const nombreVideo = video.nombre.toLowerCase();
-    if(nombreVideo.includes(termino)) {
+    if (nombreVideo.includes(termino)) {
       return video;
     } else {
       return;
     }
   })
 
-  mostrarResultados(resultados);
+  if (resultados.length > 0) {
+    mostrarResultados(resultados);
+  } else {
+    ulContainer.textContent = '';
+    mostrarAlerta('No se encontraron resultados.')
+  }
 }
 
 function mostrarResultados(resultados) {
-
   ulContainer.textContent = '';
 
-  resultados.forEach( elemento => {
+  resultados.forEach(elemento => {
     const nuevoLi = document.createElement('li');
     const pTitulo = document.createElement('p');
     const img = document.createElement('img');
     const pSinopsis = document.createElement('p');
 
     nuevoLi.className = 'w-64 bg-gray-100 border-2 border-transparent hover:border-pink-400 hover:drop-shadow-xl hover:scale-105 hover:cursor-pointer rounded-3xl transition p-4';
-    
-    
+
+
     pTitulo.className = 'font-bold text-center mb-2 capitalize text-lg';
     pTitulo.textContent = `${elemento.nombre} (${elemento.year})`
-    
+
     img.className = 'shadow-lg object-cover'
     img.src = elemento.cover;
     img.alt = `cover ${elemento.nombre} (${elemento.year})`
-    
-    pSinopsis.className = 'text-center transition-all';
+
+    pSinopsis.className = 'text-center transition-all hidden';
     pSinopsis.id = 'sinopsis';
     pSinopsis.textContent = elemento.sinopsis;
-    
+
     nuevoLi.appendChild(pTitulo)
     nuevoLi.appendChild(img)
     nuevoLi.appendChild(pSinopsis);
-    
-    
+
+
     ulContainer.appendChild(nuevoLi)
 
   })
@@ -119,14 +131,4 @@ function mostrarAlerta(mensaje) {
   setTimeout(() => {
     alerta.remove();
   }, 3000);
-}
-
-function mostrarSinopsis(e) {
-  // console.log(e.target.lastChild);
-}
-
-function ocultarSinopsis(e) {
-  // e.stopImmediatePropagation()
-  e.stopPropagation()
-  console.log('saliendo');
 }
